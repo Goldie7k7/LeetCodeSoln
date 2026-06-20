cfdcbd61bea35de42c9583bf5c5eefe33083ead0
Divyansh Changoiwal <132901844+Goldie7k7@users.noreply.github.com>
2026-06-20 23:06:32 +0530

    [704] [Time Beats: 100.00%] [Memory Beats: 99.42%] - LeetPush

---
+++ Binary-Search.py
@@ -0,0 +1,8 @@
+1class Solution:
+2    def search(self, nums: List[int], target: int) -> int:
+3        for i in range(0,len(nums)):
+4            if nums[i]==target:
+5                return i
+6        
+7        return -1
+8        
